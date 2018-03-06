77688ac9762eaecb6118e9bc0cf024a1f51fa977
y.savitski <y.savitsli@itosweb.com>
2018-03-06 14:51:02 +0300

    add simple logic

--- src/main/java/com/example/google/cloud/helloworld_goolgecloud/HelloworldGoolgecloudApplication.java
+++ src/main/java/com/example/google/cloud/helloworld_goolgecloud/HelloworldGoolgecloudApplication.java
@@ -2,11 +2,25 @@ package com.example.google.cloud.helloworld_goolgecloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class HelloworldGoolgecloudApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloworldGoolgecloudApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello World!";
+	}
+
+	@RequestMapping("/_ah/health")
+	public String healthy() {
+		// Message body required though ignored
+		return "Still surviving.";
 	}
 }
--- src/test/java/com/example/google/cloud/helloworld_goolgecloud/HelloworldGoolgecloudApplicationTests.java
+++ src/test/java/com/example/google/cloud/helloworld_goolgecloud/HelloworldGoolgecloudApplicationTests.java
@@ -2,15 +2,29 @@ package com.example.google.cloud.helloworld_goolgecloud;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.assertTrue;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SuppressWarnings("checkstyle:abbreviationaswordinname")
 public class HelloworldGoolgecloudApplicationTests {
+
+	@Autowired
+	private TestRestTemplate template;
 
 	@Test
 	public void contextLoads() {
 	}
 
+	@Test
+	public void testRequest() throws Exception {
+		ResponseEntity<String> responseEntity = template.getForEntity("/_ah/health", String.class);
+		assertTrue(responseEntity.toString().contains("surviving"));
+	}
 }
